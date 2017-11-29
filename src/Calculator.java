89d939b60c438f24f076d794199a1b6dc063c4b2
Andreas Karlsson <k.andreas@live.se>
2017-11-29 19:09:02 +0100

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,36 @@
+public class Calculator {
+    static double addition(double a, double b){
+        // Implement functionality for addition
+        return 0;
+    }
+
+    static double subtraction(double a, double b) {
+        // Implement functionality for subtraction
+        return 0;
+    }
+
+    static double multiplication(double a, double b) {
+        // Implement functionality for multiplication
+        return 0;
+    }
+
+    static double division(double a, double b) {
+        // Implement functionality for division
+        return 0;
+    }
+
+    static double powerOf(double base, int exponent) {
+        // Implement functionality for powerOf
+        return 0;
+    }
+
+    static double square(double a) {
+        // Implement functionality for square function
+        return 0;
+    }
+
+    static double root(double a) {
+        // Implement functionality for root function
+        return 0;
+    }
+}
---
+++ src/Decoder.java
@@ -0,0 +1,4 @@
+public class Decoder {
+    // Class that can decode a string of instructions into actual instructions and generate an answer
+
+}

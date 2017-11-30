7ec809610fa6b6848e0888924fa9f3e516429282
IcEiE <33426421+IcEiE@users.noreply.github.com>
2017-11-30 13:20:29 +0100

    Subtraction unitest implemented

--- src/simple_Calculator/unitTests/AllTests.java
+++ src/simple_Calculator/unitTests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PowerOfTest.class, AdditionTest.class })
+@SuiteClasses({ PowerOfTest.class, AdditionTest.class, SubtractionTest.class })
 public class AllTests {
 
 }

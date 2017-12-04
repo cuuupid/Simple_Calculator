b33f17ba82ff0ae8dc7ab9fc67b20ce8bd79fbcf
BlizzaN <p.pettersson92@gmail.com>
2017-12-04 17:50:44 +0100

    I have fixed the merged error

--- src/simple_Calculator/unitTests/AllTests.java
+++ src/simple_Calculator/unitTests/AllTests.java
@@ -5,12 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-<<<<<<< HEAD
-@SuiteClasses({ PowerOfTest.class, AdditionTest.class, SubtractionTest.class, MultiplicationTest.class, SquareTest.class, DecoderTests.class })
 
-=======
-@SuiteClasses({ PowerOfTest.class, AdditionTest.class, SubtractionTest.class, MultiplicationTest.class, SquareTest.class, RootTest.class })
->>>>>>> 5cc707f3033efb688ea8dd60bdbc06c7fd4f49d6
+@SuiteClasses({ PowerOfTest.class, AdditionTest.class, SubtractionTest.class, MultiplicationTest.class, SquareTest.class, DecoderTests.class, RootTest.class  })
+
 public class AllTests {
 
 }
--- src/simple_Calculator/unitTests/RootTest.java
+++ src/simple_Calculator/unitTests/RootTest.java
@@ -1,6 +1,5 @@
 package simple_Calculator.unitTests;
 
-<<<<<<< HEAD
 import simple_Calculator.Calculator;
 
 import static org.junit.Assert.*;
@@ -32,25 +31,3 @@ public class RootTest {
     }
 
 }
-=======
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import simple_Calculator.Calculator;
-
-public class RootTest {
-
-	@Test
-	public void test() {
-		assertEquals(5, Calculator.root(25), 0.000000001);
-		assertEquals(0, Calculator.root(0), 0.000000001);
-		assertEquals(7, Calculator.root(49), 0.000000001);
-		assertEquals(Math.sqrt(10), Calculator.root(10), 0.000000001);
-		assertEquals(Math.sqrt(0.1), Calculator.root(0.1), 0.000000001);
-		assertEquals(Math.sqrt(1000000000.9132891), Calculator.root(1000000000.9132891), 0.000000001);
-
-	}
-
-}
->>>>>>> 5cc707f3033efb688ea8dd60bdbc06c7fd4f49d6
